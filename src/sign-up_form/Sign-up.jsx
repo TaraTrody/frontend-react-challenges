1b11cd16136927efaf1bc1866d6898642dd1c4f6
Tara <taratrody@gmail.com>
2021-09-03 17:49:47 -0400

    set up sign-up component files

---
+++ src/sign-up_form/Sign-up.jsx
@@ -0,0 +1,13 @@
+
+
+const Signup = () => {
+    <div>
+        <h1> Learn to code by watching others See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</h1>
+        <button>Try it free 7 days then $20/mo. thereafter</button>
+        <div>
+            <form action=""></form>
+        </div>
+    </div>
+}a
+
+export default Signup;
