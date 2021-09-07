a17401add38a92c9fe552edbd31ead39a2cd6f60
Tara <taratrody@gmail.com>
2021-09-06 23:58:05 -0400

    prettier cleanup

--- src/home/Gallery.jsx
+++ src/home/Gallery.jsx
@@ -1,22 +1,18 @@
 import React from "react";
-import {
-    Link
-} from  'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Gallery = () => {
-    return(
-        
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/signup">Sign-up Form</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-    
-    )
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/signup">Sign-up Form</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Gallery;
