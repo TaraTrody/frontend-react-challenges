f5b1e30ed3dd691ea0c7c931e1caaee813cda56e
Tara <taratrody@gmail.com>
2021-09-08 01:28:32 -0400

    import Global Font Styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
+import FontStyles from "./fontStyles"
 
 ReactDOM.render(
   <React.StrictMode>
+    <FontStyles />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
