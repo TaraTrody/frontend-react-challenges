437e62275928474fa6db128abebcdf7669daa7c4
Tara <taratrody@gmail.com>
2021-09-09 23:58:27 -0400

    Add Overlay to background

--- src/sign-up_form/Signup.jsx
+++ src/sign-up_form/Signup.jsx
@@ -5,14 +5,16 @@ import img from "../assets/images/bg-intro-mobile.png";
 const Signup = () => {
   return (
     <Container>
-      <TitleText>
-        <h1>Learn to code by watching others</h1>
-        <p>
-          See how experienced developers solve problem in real-time. Watching
-          scripted tutorials is great, but understanding how developers think is
-          valuable
-        </p>
-      </TitleText>
+      <Overlay>
+        <TitleText>
+          <h1>Learn to code by watching others</h1>
+          <p>
+            See how experienced developers solve problem in real-time. Watching
+            scripted tutorials is great, but understanding how developers think
+            is valuable
+          </p>
+        </TitleText>
+      </Overlay>
     </Container>
   );
 };
@@ -21,15 +23,20 @@ export default Signup;
 
 const Container = styled.div`
   background: url(${img});
+  background-size: cover;
   display: grid;
   width: 375px;
   height: 100vh;
-  opacity: 1;
+  
+`;
+
+const Overlay = styled.div`
+  background: rgb(255, 121, 121, 1);
 `;
 
 const TitleText = styled.div`
   margin: 6.25em 1.56em 1.56em 3.125em;
-  
+
   h1 {
     font-weight: 700;
   }
