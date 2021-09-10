7d525f202b9eb4288231eed50b466cf711126da9
Tara <taratrody@gmail.com>
2021-09-09 23:12:54 -0400

    Add normalizing styles

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -5,8 +5,18 @@ const GlobalStyles = createGlobalStyle`
 
     ${fonts};
 
+    html {
+        box-sizing: border-box;
+    }
+
+    *, 
+    *:before, 
+    *:after {
+        box-sizing: border-box;
+    }
+
     body {
-        font-family: 'Poppins', sans-serif;
+        font-family: 'Poppins', -apple-system, BlinkMacSystemFont, system-ui, sans-serif;
     }
 
 `;
