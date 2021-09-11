5cda47c77e257c67e1c30181aa4463cf9d8d8add
Tara <taratrody@gmail.com>
2021-09-10 20:05:53 -0400

    Add wrappers around each section

--- src/sign-up_form/Signup.jsx
+++ src/sign-up_form/Signup.jsx
@@ -6,17 +6,22 @@ const Signup = () => {
   return (
     <Container>
       <Overlay>
-        <TitleText>
-          <h1>Learn to code by watching others</h1>
-          <p>
-            See how experienced developers solve problem in real-time. Watching
-            scripted tutorials is great, but understanding how developers think
-            is valuable
-          </p>
-        </TitleText>
-        <TryItBox>
-          <p>Try it free 7 days then $20/mo thereafter</p>
-        </TryItBox>
+        <TitleWrapper>
+          <TitleText>
+            <h1>Learn to code by watching others</h1>
+            <p>
+              See how experienced developers solve problem in real-time.
+              Watching scripted tutorials is great, but understanding how
+              developers think is valuable
+            </p>
+          </TitleText>
+        </TitleWrapper>
+        <BoxWrapper>
+          <Box>
+            <p>Try it free 7 days then $20/mo thereafter</p>
+          </Box>
+        </BoxWrapper>
+        <FormWrapper></FormWrapper>
       </Overlay>
     </Container>
   );
@@ -25,7 +30,6 @@ const Signup = () => {
 export default Signup;
 
 const Container = styled.div`
-  display: grid
   background-color: rgba(255, 121, 121, 1);
   width: 375px;
   height: 100vh;
@@ -37,8 +41,9 @@ const Overlay = styled.div`
   height: 100%;
 `;
 
+const TitleWrapper = styled.div``;
+
 const TitleText = styled.div`
-  margin: 6.25em 1.56em 1.56em 3.125em;
   color: #fff;
 
   h1 {
@@ -50,9 +55,13 @@ const TitleText = styled.div`
   }
 `;
 
-const TryItBox = styled.div`
+const BoxWrapper = styled.div``;
+
+const Box = styled.div`
   background-color: #5e54a4;
   width: 151px;
   height: 79px;
   color: #fff;
 `;
+
+const FormWrapper = styled.div``;
