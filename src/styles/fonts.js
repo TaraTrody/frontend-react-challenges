a15bb6c7759c3bbcf9cfebec4788d8597f160d09
Tara <taratrody@gmail.com>
2021-09-12 23:06:27 -0400

    add light font weight

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -4,6 +4,14 @@ const fonts = css`
   @font-face {
     font-family: "Poppins";
     font-style: normal;
+    font-weight: 300;
+    src: local(""), url("../fonts/poppins-v15-latin-300.woff2") format("woff2"),
+      url("../fonts/poppins-v15-latin-300.woff") format("woff"),
+      url("../fonts/poppins-v15-latin-300.ttf") format("truetype");
+  }
+  @font-face {
+    font-family: "Poppins";
+    font-style: normal;
     font-weight: 400;
     src: local(""), url("/poppins-v15-latin-regular.woff2") format("woff2"),
       url("/poppins-v15-latin-regular.woff") format("woff"),
