140c7cd41c27f4052d64ce162a3e34735586b1a4
Tara <taratrody@gmail.com>
2021-09-16 21:49:05 -0400

    create regex object to test for valid email format

---
+++ src/sign-up_form/formValidation.js
@@ -0,0 +1,10 @@
+const emailRegex = RegExp(
+  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+);
+
+export const validateForm = errors => {
+    let valid = true;
+    Object.values(errors).forEach((val) => val.length > 0 && (valid = false));
+    return valid
+}
+
