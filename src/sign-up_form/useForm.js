bb8c69ae925e554178e33b49dfe78b204b8a9c88
Tara <taratrody@gmail.com>
2021-09-25 19:18:20 -0400

    write handleChange function

--- src/sign-up_form/useForm.js
+++ src/sign-up_form/useForm.js
@@ -3,4 +3,19 @@ import { useState } from "react";
 export const useForm = (options) => {
   const [data, setData] = useState(options.initialValues || {});
   const [errors, setErrors] = useState({});
+
+  const handleChange = (key, sanitizeFn) => (e) => {
+    const value = sanitizeFn ? sanitizeFn(e.target.value) : e.target.value;
+
+    setData({
+      ...data,
+      [key]: value,
+    });
+  };
+
+  return {
+    data,
+    errors,
+    handleChange,
+  }
 };
