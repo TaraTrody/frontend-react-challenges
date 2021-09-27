8fb74d8bdcedd0a9eed128e62262800b65bf3b07
Tara <taratrody@gmail.com>
2021-09-27 00:41:22 -0400

    Complete handleSubmit Function

--- src/sign-up_form/useForm.js
+++ src/sign-up_form/useForm.js
@@ -13,9 +13,51 @@ export const useForm = (options) => {
     });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validations = options?.validations;
+
+    if (validations) {
+      let valid = true;
+      const newErrors = {};
+
+      for (const key in validations) {
+        const value = data[key];
+        const validation = validations[key];
+
+        if (validation?.required?.value && !value) {
+          valid = false;
+          newErrors[key] = validation?.required?.message;
+        }
+
+        const pattern = validation?.pattern;
+        if (pattern?.value && !RegExp(pattern.value).test(value)) {
+          valid = false;
+          newErrors[key] = pattern.message;
+        }
+
+        const custom = validation?.custom;
+        if (custom?.isValid && !custom.isValid(value)) {
+          valid = false;
+          newErrors[key] = custom.message;
+        }
+      }
+      if (!valid) {
+        setErrors(newErrors);
+        return;
+      }
+
+      setErrors({});
+      if (options?.onSubmit) {
+        options.onSubmit();
+      }
+    }
+  };
+
   return {
     data,
     errors,
     handleChange,
-  }
+    handleSubmit,
+  };
 };
