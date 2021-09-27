08a78f76788214561a1e23ecda0391a3de551e45
Tara <taratrody@gmail.com>
2021-09-26 22:59:15 -0400

    Update email regex pattern

--- src/sign-up_form/Form.jsx
+++ src/sign-up_form/Form.jsx
@@ -31,7 +31,7 @@ const Form = () => {
           message: "Email cannot be empty",
         },
         pattern: {
-          value: "^[A-Za-z]*$",
+          value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
           message: "Looks like this is not an email",
         },
       },
