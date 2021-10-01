9e2b11ce176c01061269f3525558b9a0c63d9216
Tara <taratrody@gmail.com>
2021-10-01 15:47:37 -0400

    imported & wrapped App in BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import GlobalStyles from "./GlobalStyles";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
-  
