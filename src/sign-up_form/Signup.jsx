0335488accf7edd1a81db58971320d62e0440253
Tara <taratrody@gmail.com>
2021-10-01 23:38:18 -0400

    update import stmt for styled-components

--- src/sign-up_form/Signup.jsx
+++ src/sign-up_form/Signup.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled from "styled-components/macro";
 import img from "../assets/images/bg-intro-mobile.png";
 
 import Form from "./Form";
