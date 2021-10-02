0ec8baf307854b1994abcbbe0ea046ba7e7f8cb9
Tara <taratrody@gmail.com>
2021-10-01 22:17:44 -0400

    remove unused Switch component from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 import Gallery from "./home";
 import Signup from "./sign-up_form";
