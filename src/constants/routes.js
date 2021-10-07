1532db463623da67cdbde3c6556b11c3d377b582
Tara <taratrody@gmail.com>
2021-10-06 22:47:58 -0400

    correct spelling error

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,5 @@
 export const SIGN_UP = "/";
 export const SIGN_IN = "/signin";
-export const HOME = "/home"
+export const HOME = "/home";
 export const ACCOUNT = "/account";
-export const PASSWORD_FOREGET = "/pw-forget";
+export const PASSWORD_FORGET = "/pw-forget";
