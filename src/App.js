ff8db26f24dab69d628991f27c734152955b39a5
Tara <taratrody@gmail.com>
2021-10-06 21:43:39 -0400

    update folder name

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
-import Gallery from "./home";
 import Signup from "./sign-up_form";
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={Gallery} exact />
       <Route path="/signup" component={Signup} exact />
     </div>
   );
