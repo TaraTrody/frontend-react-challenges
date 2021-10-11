4bd24de03b43d1787177ec8d3cc6b954caafde9b
Tara <taratrody@gmail.com>
2021-10-11 15:31:37 -0400

    create Firebase Context and add Provider component at top level

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import GlobalStyles from "./styles/GlobalStyles";
+import Firebase, { FirebaseContext } from "./components/Firebase";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={new Firebase()}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
