578f8ff4c7cff42820a351e53568f5b4a88ee72c
Tara <taratrody@gmail.com>
2021-10-12 22:16:20 -0400

    move validations out of Form component into separate file

--- src/components/SignUp/Form.jsx
+++ src/components/SignUp/Form.jsx
@@ -2,59 +2,10 @@ import React from "react";
 import styled, { css } from "styled-components/macro";
 import { useForm } from "./useForm";
 import icon from "../../assets/icons/icon-error.svg";
+import { validations } from "../../formValidations";
 
 const Form = () => {
-  const { data, errors, handleSubmit, handleChange } = useForm({
-    validations: {
-      firstName: {
-        required: {
-          value: true,
-          message: "First Name cannot be empty",
-        },
-        pattern: {
-          value: "^[A-Za-z]*$",
-          message: "Only letters allowed",
-        },
-      },
-      lastName: {
-        required: {
-          value: true,
-          message: "Last Name cannot be empty",
-        },
-        pattern: {
-          value: "^[A-Za-z]*$",
-          message: "Only letters allowed",
-        },
-      },
-      email: {
-        required: {
-          value: true,
-          message: "Email cannot be empty",
-        },
-        pattern: {
-          value:
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-          message: "Looks like this is not an email",
-        },
-      },
-      password: {
-        required: {
-          value: true,
-          message: "Password cannot be empty",
-        },
-        custom: {
-          isValid: (value) => (value === undefined ? false : value.length >= 8),
-          message: "Password needs to be at least 8 characters",
-        },
-      },
-    },
-    onSubmit: () => {
-      alert("User submitted");
-      setTimeout(() => {
-        window.location.reload();
-      }, 100);
-    },
-  });
+  const { data, errors, handleSubmit, handleChange } = useForm(validations);
 
   return (
     <FormContainer>
