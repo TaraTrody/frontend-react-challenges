ea6de76798dd8ada0a18459c62330cee2e9ca08f
Tara <taratrody@gmail.com>
2021-10-14 23:36:59 -0400

    import & export Form elements

---
+++ src/components/Form/index.js
@@ -0,0 +1,8 @@
+import Card from "./Card";
+import StyledForm from "./StyledForm";
+import Input from "./Input";
+import Error from "./Error";
+import Terms from "./Terms";
+import Button from "./Button";
+
+export { Card, StyledForm, Input, Error, Terms, Button };
