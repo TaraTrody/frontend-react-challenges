0c77907eb781bcf84325b899f85193edcbd0fd2d
Tara <taratrody@gmail.com>
2021-10-16 15:17:15 -0400

    remove comments

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import React from "react";
 import AppRouter from "./components/AppRouter";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-
-// import Signup from "./components/SignUp";
-// import * as ROUTES from "./constants/routes";
 
 function App() {
-  return (
-    <AppRouter />
-
-    // <Router>
-    //   <div className="App">
-    //     <Route path={ROUTES.SIGN_UP} component={Signup} exact />
-    //   </div>
-    // </Router>
-  );
+  return <AppRouter />;
 }
 
 export default App;
