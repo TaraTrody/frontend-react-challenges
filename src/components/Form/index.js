6807b6c23a941b4255b1f5f1dd2bd43c82a5fc4d
Tara <taratrody@gmail.com>
2021-11-02 01:33:14 -0400

    imported/exported FormGroup component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -4,5 +4,6 @@ import Input from "./Input";
 import Error from "./Error";
 import Terms from "./Terms";
 import Button from "./Button";
+import FormGroup from "./FormGroup";
 
-export { Card, StyledForm, Input, Error, Terms, Button };
+export { Card, StyledForm, Input, Error, Terms, Button, FormGroup };
