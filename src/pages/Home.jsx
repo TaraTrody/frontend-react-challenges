59375f23b57bf3a180ff31722dcdb54824f3bde1
Tara <taratrody@gmail.com>
2021-11-02 01:30:06 -0400

    Added Form Group Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,53 +8,70 @@ import {
   Input,
   StyledForm,
   Terms,
+  FormGroup,
 } from "../components/Form";
 import { validation } from "../validations/formValidations";
-import useForm from "../hooks/useForm";
+import useForm from "../hooks/useForm.ts";
+
+const onSubmit = () => console.log("submitted!!!");
 
 export default function Home() {
-  const { data, errors, handleSubmit, handleChange } = useForm(validation);
+  const { data, errors, handleSubmit, handleChange } = useForm(
+    validation,
+    onSubmit
+  );
 
   return (
     <Container>
       <Layout>
         <Card>
-          <StyledForm onSubmit={handleSubmit}>
-            <Input
-              correct={!errors.firstName && true}
-              type="text"
-              id="firstName"
-              placeholder={errors.firstName ? "" : "First Name"}
-              value={data.firstName || ""}
-              onChange={handleChange("firstName")}
-            />
-            {errors.firstName && <Error>{errors.firstName}</Error>}
-            <Input
-              correct={!errors.lastName && true}
-              type="text"
-              id="lastName"
-              placeholder={errors.lastName ? "" : "Last Name"}
-              value={data.lastName || ""}
-              onChange={handleChange("lastName")}
-            />
-            {errors.lastName && <Error>{errors.lastName}</Error>}
-            <Input
-              correct={!errors.email && true}
-              type="email"
-              id="email"
-              placeholder={errors.email ? "email@example.com" : "Email Address"}
-              value={data.email || ""}
-              onChange={handleChange("email")}
-            />
-            {errors.email && <Error>{errors.email}</Error>}
-            <Input
-              correct={!errors.password && true}
-              type="password"
-              id="password"
-              placeholder={errors.password ? "" : "Password"}
-              value={data.password || ""}
-              onChange={handleChange("password")}
-            />
+          <StyledForm onSubmit={handleSubmit} noValidate>
+            <FormGroup>
+              <Input
+                correct={!errors.firstName && true}
+                type="text"
+                id="firstName"
+                placeholder={errors.firstName ? "" : "First Name"}
+                value={data.firstName || ""}
+                onChange={handleChange("firstName")}
+              />
+              {errors.firstName && <Error>{errors.firstName}</Error>}
+            </FormGroup>
+            <FormGroup>
+              <Input
+                correct={!errors.lastName && true}
+                type="text"
+                id="lastName"
+                placeholder={errors.lastName ? "" : "Last Name"}
+                value={data.lastName || ""}
+                onChange={handleChange("lastName")}
+              />
+              {errors.lastName && <Error>{errors.lastName}</Error>}
+            </FormGroup>
+            <FormGroup>
+              <Input
+                correct={!errors.email && true}
+                type="email"
+                id="email"
+                placeholder={
+                  errors.email ? "email@example.com" : "Email Address"
+                }
+                value={data.email || ""}
+                onChange={handleChange("email")}
+              />
+              {errors.email && <Error>{errors.email}</Error>}
+            </FormGroup>
+            <FormGroup>
+              <Input
+                correct={!errors.password && true}
+                type="password"
+                id="password"
+                placeholder={errors.password ? "" : "Password"}
+                value={data.password || ""}
+                onChange={handleChange("password")}
+              />
+              {errors.password && <Error>{errors.password}</Error>}
+            </FormGroup>
             <Button type="submit">CLAIM YOUR FREE TRIAL</Button>
             <Terms />
           </StyledForm>
