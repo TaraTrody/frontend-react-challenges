3de0b8e14ea1b8049d608627fea9c035ca51e145
Tara <taratrody@gmail.com>
2021-11-03 23:57:50 -0400

    Add form to forgot-password page

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import Container from '../components/Container';
+import React from "react";
+import Container from "../components/Container";
+import { Card, StyledForm, FormGroup, Input, Error } from "../components/Form";
+import useForm from "../hooks/useForm.ts";
+import { validation } from "../validations/formValidations";
+
+const onSubmit = () => console.log("submitted!!!");
 
 export default function ForgotPassword() {
-    return (
-        <Container>
-
-        </Container>
-    )
+  const { data, errors, handleSubmit, handleChange } = useForm(
+    validation,
+    onSubmit
+  );
+  return (
+    <Container>
+      <Card>
+        <StyledForm onSubmit={handleSubmit} noValidate>
+          <FormGroup>
+            <Input
+              correct={!errors.email && true}
+              type="email"
+              id="email"
+              placeholder={errors.email ? "email@example.com" : "Email Address"}
+              value={data.email || ""}
+              onChange={handleChange("email")}
+            />
+            {errors.email && <Error>{errors.email}</Error>}
+          </FormGroup>
+        </StyledForm>
+      </Card>
+    </Container>
+  );
 }
