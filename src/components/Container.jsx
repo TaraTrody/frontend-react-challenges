ce130b16bc76a84a086ca873b1031b65a5b06f0a
Tara <taratrody@gmail.com>
2021-11-03 23:57:14 -0400

    create object for grid template styles

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,15 +3,24 @@ import styled from "styled-components/macro";
 import img from "../assets/images/bg-intro-mobile.png";
 
 export default function Container(props) {
-  return <Background>{props.children}</Background>;
+  return <Background {...props}>{props.children}</Background>;
 }
+
+const gridStyle = {
+  multi: {
+    mobile: "2fr auto 3fr",
+    desktop: { col: "50% 50%", row: "25% 75%" },
+  },
+  single: "1fr",
+};
 
 const Background = styled.div`
   background-color: #ff7979;
   background-image: url(${img});
   min-width: 375px;
   display: grid;
-  grid-template-rows: 2fr auto 3fr;
+  grid-template-rows:${(prop) =>
+    prop.gridAreas === "multi?" ? gridStyle.multi.mobile : gridStyle.single} 
   justify-content: center;
   align-items: center;
 
@@ -19,8 +28,10 @@ const Background = styled.div`
     display: grid;
     width: 100%;
     min-height: 100vh;
-    grid-template-columns: 50% 50%;
-    grid-template-rows: 25% 75%;
+    grid-template-columns: ${(prop) =>
+      prop.gridAreas === "multi?" ? gridStyle.multi.desktop : gridStyle.single} 
+    grid-template-rows: ${(prop) =>
+      prop.gridAreas === "multi?" ? gridStyle.multi.desktop : gridStyle.single} 
     overflow: auto;
   }
 `;
