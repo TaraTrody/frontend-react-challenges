bd79ba7df376b3720ae7ac137d1d2698fcef7f8c
Tara <taratrody@gmail.com>
2022-02-15 15:05:21 -0500

    remove on submit property and outer brackets

--- src/validations/formValidations.js
+++ src/validations/formValidations.js
@@ -1,51 +1,43 @@
 export const validations = {
-  // validations: {
-    firstName: {
-      required: {
-        value: true,
-        message: "First Name cannot be empty",
-      },
-      pattern: {
-        value: "^[A-Za-z]*$",
-        message: "Only letters allowed",
-      },
+  firstName: {
+    required: {
+      value: true,
+      message: "First Name cannot be empty",
     },
-    lastName: {
-      required: {
-        value: true,
-        message: "Last Name cannot be empty",
-      },
-      pattern: {
-        value: "^[A-Za-z]*$",
-        message: "Only letters allowed",
-      },
+    pattern: {
+      value: "^[A-Za-z]*$",
+      message: "Only letters allowed",
     },
-    email: {
-      required: {
-        value: true,
-        message: "Email cannot be empty",
-      },
-      pattern: {
-        value:
-          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        message: "Looks like this is not an email",
-      },
+  },
+  lastName: {
+    required: {
+      value: true,
+      message: "Last Name cannot be empty",
     },
-    password: {
-      required: {
-        value: true,
-        message: "Password cannot be empty",
-      },
-      custom: {
-        isValid: (value) => (value === undefined ? false : value.length >= 8),
-        message: "Password needs to be at least 8 characters",
-      },
+    pattern: {
+      value: "^[A-Za-z]*$",
+      message: "Only letters allowed",
     },
-  }
-  // onSubmit: () => {
-  //   console.log("user Submitted");
-  //   alert("User submitted");
-  //   setTimeout(() => {
-  //     window.location.reload();
-  //   }, 100);
-  // },
+  },
+  email: {
+    required: {
+      value: true,
+      message: "Email cannot be empty",
+    },
+    pattern: {
+      value:
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      message: "Looks like this is not an email",
+    },
+  },
+  password: {
+    required: {
+      value: true,
+      message: "Password cannot be empty",
+    },
+    custom: {
+      isValid: (value) => (value === undefined ? false : value.length >= 8),
+      message: "Password needs to be at least 8 characters",
+    },
+  },
+};
