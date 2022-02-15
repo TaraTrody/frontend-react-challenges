7a389b13f099d752d63428fb65a873e553136509
Tara <taratrody@gmail.com>
2022-02-15 15:06:46 -0500

    change file ext to .tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import Layout from "../components/Layout";
+import Container from "../components/Container";
+import {
+  Card,
+  Button,
+  Error,
+  Input,
+  StyledForm,
+  Terms,
+  FormGroup,
+} from "../components/Form";
+import { validations } from "../validations/formValidations";
+import useForm from "../hooks/useForm";
+import useWindowSize from "../hooks/useWindowSize";
+
+const onSubmit = () => {
+  console.log("user Submitted");
+  alert("User submitted");
+  setTimeout(() => {
+    window.location.reload();
+  }, 100);
+};
+export default function Home() {
+  const { data, errors, handleSubmit, handleChange } = useForm({
+    validations,
+    onSubmit,
+  });
+
+  const clientWidth = useWindowSize();
+  console.log(clientWidth);
+  return (
+    <Container gridAreas="multi">
+      <Layout>
+        <Card>
+          <StyledForm onSubmit={handleSubmit} noValidate>
+            <FormGroup>
+              <Input
+                correct={!errors.firstName && true}
+                type="text"
+                id="firstName"
+                placeholder={errors.firstName ? "" : "First Name"}
+                value={data.firstName || ""}
+                onChange={handleChange("firstName")}
+              />
+              {errors.firstName && <Error>{errors.firstName}</Error>}
+            </FormGroup>
+            <FormGroup>
+              <Input
+                correct={!errors.lastName && true}
+                type="text"
+                id="lastName"
+                placeholder={errors.lastName ? "" : "Last Name"}
+                value={data.lastName || ""}
+                onChange={handleChange("lastName")}
+              />
+              {errors.lastName && <Error>{errors.lastName}</Error>}
+            </FormGroup>
+            <FormGroup>
+              <Input
+                correct={!errors.email && true}
+                type="email"
+                id="email"
+                placeholder={
+                  errors.email ? "email@example.com" : "Email Address"
+                }
+                value={data.email || ""}
+                onChange={handleChange("email")}
+              />
+              {errors.email && <Error>{errors.email}</Error>}
+            </FormGroup>
+            <FormGroup>
+              <Input
+                correct={!errors.password && true}
+                type="password"
+                id="password"
+                placeholder={errors.password ? "" : "Password"}
+                value={data.password || ""}
+                onChange={handleChange("password")}
+              />
+              {errors.password && <Error>{errors.password}</Error>}
+            </FormGroup>
+            <Button type="submit">CLAIM YOUR FREE TRIAL</Button>
+            <Terms />
+          </StyledForm>
+        </Card>
+      </Layout>
+    </Container>
+  );
+}
