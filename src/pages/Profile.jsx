8b2b35b4035a850d857765d7d35e62518ee52108
Tara <taratrody@gmail.com>
2022-02-19 23:59:15 -0500

    add user variable

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,6 +3,7 @@ import Container from "../components/Container";
 import styled from "styled-components/macro";
 
 export default function Profile() {
+  const user = "Tara";
   return (
     <Container gridAreas={"single"}>
       <Text>Welcome Back, {user}</Text>
