6110f380a31d39e7e27482cbfd732710754ef329
Tara <taratrody@gmail.com>
2022-02-20 23:14:02 -0500

    create and export Firebase Auth instance

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -8,8 +8,8 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
-
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
