17b91ea684e2bef3e1afc4220523e5a8de483f5c
Tara <taratrody@gmail.com>
2022-02-20 23:17:11 -0500

    create and export auth context provider

--- src/contexts/authContexts.js
+++ src/contexts/authContexts.js
@@ -1,15 +1,41 @@
 import { createContext, useContext, useEffects, useState } from "react";
-
+import { auth } from "../utils/firebase";
 import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signOut
+  getAuth,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 
-export const AuthContext  = createContext({
-    currentUser: null,
-    register: () => Promise,
-})
+export const AuthContext = createContext({
+  currentUser: null,
+  register: () => Promise,
+});
 
 export const useAuth = () => useContext(AuthContext);
+
+export default function AuthContextProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffects(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const register = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const value = {
+    currentUser,
+    register,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
